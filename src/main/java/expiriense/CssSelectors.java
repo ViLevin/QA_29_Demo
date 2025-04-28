24f287f0bd42c6846228e6ab21a0610cc327fb3a
ViLevin <vilevinqa@gmail.com>
2025-04-28 19:26:10 +0300

    Added XpathSelectors

--- src/main/java/expiriense/CssSelectors.java
+++ src/main/java/expiriense/CssSelectors.java
@@ -1,6 +1,7 @@
 package expiriense;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +15,8 @@ public class CssSelectors {
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
 
+        hideBanner();
+        hideFooter();
         WebElement footer = driver.findElement(By.tagName("footer"));
         System.out.println(footer.getAttribute("class"));
 
@@ -25,10 +28,28 @@ public class CssSelectors {
         elements.click();
 
         pause(5);
+        hideFooter();
+        WebElement radioButton = driver.findElement(By.id("item-2"));
+        radioButton.click();
+        pause(5);
+        //WebElement buttonYes = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+        WebElement buttonYes = driver.findElement(By.xpath("//label[@for='yesRadio']"));
+        buttonYes.click();
 
-        driver.quit();
+        //driver.quit();
 
     }
+
+    public void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+
+    public void hideFooter(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
+    }
+
     public void pause(int time){
         try {
             Thread.sleep(4000);
