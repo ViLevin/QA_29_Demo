9f919fc5549e77d975b789817f7c931cd82c696c
ViLevin <vilevinqa@gmail.com>
2025-05-07 16:07:28 +0300

    Crearted App, Pages

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -20,8 +20,8 @@ public class AppManager {
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null)
-            driver.quit();
+//        if (driver != null)
+//            driver.quit();
     }
 
 
