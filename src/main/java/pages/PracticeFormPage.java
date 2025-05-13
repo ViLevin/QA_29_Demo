87719c0ca38b533cc3f7cc68f35ff1b3f55b8165
ViLevin <vilevinqa@gmail.com>
2025-05-13 23:30:52 +0300

    Web_6

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -1,11 +1,17 @@
 package pages;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.List;
 
 public class PracticeFormPage extends BasePage {
     public PracticeFormPage(WebDriver driver) {
@@ -23,15 +29,16 @@ public class PracticeFormPage extends BasePage {
     WebElement fieldMobile;
     @FindBy(xpath = "//textarea[@id='currentAddress']")
     WebElement fieldAddress;
-    @FindBy(id="dateOfBirth")
+    @FindBy(id = "dateOfBirthInput")
     WebElement fieldDateOfBirth;
-    @FindBy(id="react-select-3-input")
+    @FindBy(id = "subjectsInput")
+    WebElement fieldSubjects;
+    @FindBy(id = "react-select-3-input")
     WebElement inputState;
-    @FindBy(id="react-select-4-input")
+    @FindBy(id = "react-select-4-input")
     WebElement inputCity;
     @FindBy(xpath = "//button[text()='Submit']")
     WebElement btnSubmit;
-
 
 
     public void typePracticeForm(Student student) {
@@ -39,11 +46,59 @@ public class PracticeFormPage extends BasePage {
         fieldName.sendKeys(student.getName());
         fieldLastName.sendKeys(student.getLastName());
         fieldEmail.sendKeys(student.getEmail());
+        typeGender(student.getGender());
         fieldMobile.sendKeys(student.getMobile());
+//        fieldDateOfBirth.sendKeys(student.getDateOfBirth());
+        typeDateOfBirth(student.getDateOfBirth());
+        typeSubjects(student.getSubject());
+        typeHobbies(student.getHobbies());
         fieldAddress.sendKeys(student.getAddress());
-        fieldDateOfBirth.sendKeys(student.getDateOfBirth());
-        inputState.sendKeys(student.getState());
-        inputCity.sendKeys(student.getCity());
-        btnSubmit.click();
+//        inputState.sendKeys(student.getState());
+//        inputCity.sendKeys(student.getCity());
+//        btnSubmit.click();
+    }
+
+    private void typeDateOfBirth(String dateOfBirth) {
+        fieldDateOfBirth.click();
+        String operationSystem = System.getProperty("os.name");
+        System.out.println(operationSystem);
+        if(operationSystem.startsWith("Windows 11"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        else  if(operationSystem.startsWith("Mac"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        fieldDateOfBirth.sendKeys(dateOfBirth);
+        fieldDateOfBirth.sendKeys(Keys.ENTER);
+
+    }
+
+    private void typeSubjects(String subjects){
+        fieldSubjects.click();
+        String[] arr = subjects.split(",");
+        for (String s: arr){
+            fieldSubjects.sendKeys(s);
+            fieldSubjects.sendKeys(Keys.ENTER);
+        }
+    }
+
+
+    private void typeGender(Gender gender) {
+        WebElement btnGender = driver.findElement(By.xpath(gender.getLocator()));
+        btnGender.click();
+    }
+
+    private void typeHobbies(List<Hobbies> hobbies){
+        for (Hobbies h:hobbies){
+            switch (h){
+                case MUSIC:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case SPORTS:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case READING:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+            }
+        }
     }
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,16 +1,26 @@
 package tests;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
     @Test
     public void studentRegFormPositiveTest() {
-        Student student = new Student("Vasya", "Vasylev", "111@gmail.com", "Other", "1234567890", "11 Dec 2004", "Maths,Physics,Chemistry", "sport", "", "Street,1", "NCR", "Delhi");
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.MUSIC);
+        hobbies.add(Hobbies.SPORTS);
+
+
+        Student student = new Student("Vasya", "Vasylev", "111@gmail.com", Gender.OTHER, "1234567890", "11 Dec 2004", "Maths,Physics,Chemistry", hobbies, "", "Street,1", "NCR", "Delhi");
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
         new PracticeFormPage(getDriver()).typePracticeForm(student);
